0790b60c7b9e0df8ba1c56661a56c90fb2df0992
dncryptox-cpu <dncryptox@gmail.com>
2026-09-12 09:31:44 +0700

    fix(goagents): resolve image breaks, update contact info, add fallback image & build production CSS

--- goagents/js/config.js
+++ goagents/js/config.js
@@ -9,11 +9,11 @@ window.GOAGENTS_CONFIG = {
   // Shop & Contact Information
   SHOP_NAME: "Goagents",
   SHOP_TAGLINE: "Tài khoản AI & Subscription giá rẻ, nâng cấp tự động, bảo hành uy tín 1:1",
-  ZALO_PHONE: "0901234567",
-  ZALO_URL: "https://zalo.me/0901234567",
+  ZALO_PHONE: "0398057191",
+  ZALO_URL: "https://zalo.me/0398057191",
   TELEGRAM_URL: "https://t.me/goagents_support",
   FANPAGE_URL: "https://facebook.com/goagents.official",
-  HOTLINE: "090.123.4567",
+  HOTLINE: "0398.057.191",
   WORKING_HOURS: "08:00 - 23:00 (Hàng ngày)",
 
   // Response Time Expectation
